961520d0d5d3c6f901face4df0e092937157c1b5
shaojunda <shaojunda@gmail.com>
2020-03-27 16:34:59 +0800

    feat: add sync info model

---
+++ app/models/sync_info.rb
@@ -0,0 +1,15 @@
+class SyncInfo < ApplicationRecord
+  enum status: { pending: 0, completed: 1, forked: 2 }
+end
+
+# == Schema Information
+#
+# Table name: sync_infos
+#
+#  id               :bigint           not null, primary key
+#  status           :integer          default("pending")
+#  tip_block_hash   :string
+#  tip_block_number :bigint
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
---
+++ db/migrate/20200327083013_create_sync_infos.rb
@@ -0,0 +1,11 @@
+class CreateSyncInfos < ActiveRecord::Migration[6.0]
+  def change
+    create_table :sync_infos do |t|
+      t.bigint :tip_block_number
+      t.string :tip_block_hash
+      t.integer :status, default: 0
+
+      t.timestamps
+    end
+  end
+end
