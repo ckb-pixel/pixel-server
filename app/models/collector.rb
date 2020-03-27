ed6377adb1e48842c28335b41c32bfcf9ad01828
shaojunda <shaojunda@gmail.com>
2020-03-28 01:42:16 +0800

    feat: add collector

---
+++ app/models/collector.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class Collector
+  def default_scanner(lock_hash)
+    outputs = []
+    output_index = 0
+    page = 0
+    total_pages = Output.where(lock_hash: lock_hash).page(page).per(100).total_pages
+    Enumerator.new do |result|
+      loop do
+        if output_index < outputs.size
+          result << outputs[output_index]
+          output_index += 1
+        else
+          output_index = 0
+          outputs = Output.where(lock_hash: lock_hash).live.page(page).per(100).to_a
+          page += 1
+          puts "page: #{page}"
+        end
+        raise StopIteration if page > total_pages
+      end
+    end
+  end
+end
