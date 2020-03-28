5062a30379189b4ef12e0566396f40b9a944648f
shaojunda <shaojunda@gmail.com>
2020-03-28 12:56:29 +0800

    chore: add generate fake pixel cells for ui test

--- app/controllers/api/v1/pixel_cells_controller.rb
+++ app/controllers/api/v1/pixel_cells_controller.rb
@@ -3,7 +3,7 @@
 class Api::V1::PixelCellsController < ApplicationController
   before_action :pagination_params, only: :index
   def index
-    pixel_cells = Output.pixel.live.page(@page).per(@page_size)
+    pixel_cells = generate_fake_pixel_cells # Output.pixel.live.page(@page).per(@page_size)
     render json: OutputSerializer.new(pixel_cells)
   end
 
@@ -11,5 +11,27 @@ class Api::V1::PixelCellsController < ApplicationController
     def pagination_params
       @page = params[:page] || 1
       @page_size = params[:page_size] || Output.default_per_page
+    end
+
+    def generate_fake_pixel_cells
+      outputs = []
+      (0..784).step(16).each do |x|
+        (0..784).step(16).each do |y|
+          r, g, b = rand(0..255), rand(0..255), rand(0..255)
+          output =
+            Output.new(
+              block_hash: "0x#{SecureRandom.hex(32)}", capacity: 666*10**8, cell_index: 0, cell_type: "pixel",
+              cellbase: false, epoch_number: rand(100), lock_args: "0x#{SecureRandom.hex(32)}",
+              lock_code_hash: "0x#{SecureRandom.hex(32)}", lock_hash_type: "data", status: "live",
+              tx_hash: "0x#{SecureRandom.hex(32)}", type_args: "0x#{SecureRandom.hex(32)}", type_code_hash: "0x#{SecureRandom.hex(32)}",
+              type_hash_type: "data"
+            )
+          output.data = output.generate_pixel_data(x, y, r, g, b)
+          output.output_data_len = CKB::Utils.hex_to_bin(output.data).bytesize
+          outputs << output
+        end
+      end
+
+      outputs
     end
 end
